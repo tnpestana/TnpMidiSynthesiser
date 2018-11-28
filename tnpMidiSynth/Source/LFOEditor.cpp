19464d2fb36bd3f2e655ce94876d2f4eee26c34f
Tiago Pestana <tnevespestana@gmail.com>
2018-11-28 17:07:22 +0000

    specify LFO combo box text color

--- tnpMidiSynth/Source/LFOEditor.cpp
+++ tnpMidiSynth/Source/LFOEditor.cpp
@@ -53,7 +53,7 @@ LFOEditor::~LFOEditor()
 void LFOEditor::paint(Graphics &)
 {
 	labelTitle.setColour(Label::backgroundColourId, Colours::cadetblue);
-
+	comboOscType.setColour(ComboBox::textColourId, Colours::black);
 	sliderLfoDepth.setColour(Slider::textBoxTextColourId, Colours::black);
 	sliderLfoRate.setColour(Slider::textBoxTextColourId, Colours::black);
 }
