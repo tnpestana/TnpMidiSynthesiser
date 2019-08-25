c0205b8e03aae023acddb696f1a18524eec275f1
Tiago Pestana <tnevespestana@gmail.com>
2019-08-25 18:20:32 +0100

    master editor

---
+++ tnpMidiSynth/Source/MasterEditor.cpp
@@ -0,0 +1,83 @@
+/*
+  ==============================================================================
+
+    MasterEditor.cpp
+    Created: 25 Aug 2019 5:18:55pm
+    Author:  Tiago Pestana
+
+  ==============================================================================
+*/
+
+#include "MasterEditor.h"
+
+MasterEditor::MasterEditor(TnpMidiSynthAudioProcessor& p)
+//  Processor references
+: processor (p),
+treeState(p.getTreeState()),
+//  Parameter attachments
+gainAttachment(std::make_unique<AudioProcessorValueTreeState::SliderAttachment>
+               (treeState, "gain", gainSlider)),
+attNumVoices(std::make_unique<AudioProcessorValueTreeState::ComboBoxAttachment>
+(treeState, "numVoices", numVoicesInput))
+{
+    addAndMakeVisible(numVoicesLabel);
+    addAndMakeVisible(numVoicesInput);
+    addAndMakeVisible(labelTitle);
+    addAndMakeVisible(gainSlider);
+    addAndMakeVisible(gainLabel);
+    
+    labelTitle.setText("MASTER", dontSendNotification);
+    gainLabel.setText("gain", dontSendNotification);
+    numVoicesLabel.setText("voices: ", dontSendNotification);
+    
+    labelTitle.setJustificationType(Justification::centred);
+    gainLabel.setJustificationType(Justification::centredBottom);
+    numVoicesLabel.setJustificationType(Justification::centredRight);
+    
+    gainSlider.setSliderStyle(Slider::RotaryVerticalDrag);
+    gainSlider.setTextBoxStyle(Slider::TextBoxBelow, false, 50, 15);
+    gainSlider.setTextValueSuffix(" dB");
+    
+    //  Populate combo boxes with strings stored as parameter choices
+    if (auto* choiceParameter = dynamic_cast<AudioParameterChoice*>(treeState.getParameter("numVoices")))
+    {
+        numVoicesInput.addItemList(choiceParameter->choices, 1);
+        numVoicesInput.setSelectedId(choiceParameter->getIndex() + 1);
+    }
+}
+
+MasterEditor::~MasterEditor()
+{
+}
+
+void MasterEditor::paint(Graphics &)
+{
+    labelTitle.setColour(Label::backgroundColourId, Colours::lightgrey);
+    labelTitle.setColour(Label::outlineColourId, Colours::black);
+    
+    numVoicesInput.setColour(ComboBox::textColourId, Colours::black);
+    
+    gainSlider.setColour(Slider::textBoxTextColourId, Colours::black);
+}
+
+void MasterEditor::resized()
+{
+    const int labelHeight = 15;
+    
+    juce::Rectangle<int> area (getLocalBounds());
+    juce::Rectangle<int> titleSection (area.removeFromTop(20));
+    labelTitle.setBounds (titleSection.reduced(2));
+    
+    juce::Rectangle<int> topSection (area.removeFromTop(area.getHeight() * 0.30));
+    juce::Rectangle<int> slidersSection (area);
+    
+    numVoicesLabel.setBounds(topSection.removeFromTop(20));
+    numVoicesInput.setBounds(topSection.reduced(2));
+    
+//    numVoicesLabel.setBounds (topSection.removeFromLeft(topSection.getWidth() / 5));
+//    numVoicesInput.setBounds (topSection.reduced(2));
+    
+    juce::Rectangle<int> filterGainFactorSection (slidersSection.removeFromLeft(slidersSection.getWidth() / 4));
+    gainLabel.setBounds (filterGainFactorSection.removeFromTop(labelHeight));
+    gainSlider.setBounds (filterGainFactorSection);
+}
---
+++ tnpMidiSynth/Source/MasterEditor.h
@@ -0,0 +1,38 @@
+/*
+  ==============================================================================
+
+    MasterEditor.h
+    Created: 25 Aug 2019 5:18:55pm
+    Author:  Tiago Pestana
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "PluginProcessor.h"
+
+class MasterEditor : public Component
+{
+public:
+    MasterEditor(TnpMidiSynthAudioProcessor&);
+    ~MasterEditor();
+    
+private:
+    void paint(Graphics&) override;
+    void resized() override;
+    
+    TnpMidiSynthAudioProcessor& processor;
+    AudioProcessorValueTreeState& treeState;
+    
+    Label labelTitle;
+    Label numVoicesLabel;
+    Label gainLabel;
+    
+    ComboBox numVoicesInput;
+    Slider gainSlider;
+    
+    std::unique_ptr<AudioProcessorValueTreeState::SliderAttachment> gainAttachment;
+    std::unique_ptr<AudioProcessorValueTreeState::ComboBoxAttachment> attNumVoices;
+};
